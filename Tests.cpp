49af2248650283d72efca78bc8b07d8183de77e6
Kristjan Kongas <kongaskristjan@gmail.com>
2020-02-19 20:38:40 +0200

    Add gtest

---
+++ Tests.cpp
@@ -0,0 +1,8 @@
+
+#include <gtest/gtest.h>
+#include "algo.h"
+
+TEST(Example, Case) {
+    EXPECT_EQ(1, one());
+}
+
---
+++ algo.h
@@ -0,0 +1,8 @@
+#ifndef __ALGO_H__
+#define __ALGO_H__
+
+int one() {
+    return 1;
+}
+
+#endif
